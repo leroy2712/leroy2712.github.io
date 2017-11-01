fe314a5b39004d45bdd47f33d899fe61688c3e5b
Leroy Omondi <leroyomondi@gmail.com>
2017-11-01 11:01:20 +0300

    probably done

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ $("a").on('click', function (event) {
         });
     }
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
